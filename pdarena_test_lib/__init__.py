5445a9a41d4c3d9e787c90f0905b880115a8e204
celeste <parafactual@gmail.com>
2023-01-05 16:41:23 -0800

    Add all basic functionality

--- pdarena_test_lib/__init__.py
+++ pdarena_test_lib/__init__.py
@@ -1,8 +1,51 @@
+from dataclasses import dataclass
+from enum import Enum
+from time import time
+from typing import Callable, List, Union
+
+# should_defect function
+Bot = Callable[['Bot', List[bool]], bool]
+
+
+class Result(Enum):
+    VALID = 0
+    TIMEOUT = 1
+    EXCEPTION = 2
+
+
+@dataclass
+class ResultDetails:
+    result: Result
+    exception: Union[Exception, None]
+
+
+def tit_for_tat(self, opponent: Bot, history: List[bool]) -> bool:
+    if len(history) == 0:
+        return False
+    return history[-1]
+
+
 class Validator:
-    def __init__(self, testcases, n_matchups=10, n_rounds=10):
+    def __init__(self, n_matchups: int, n_rounds: int, testcases: List[Bot] = [tit_for_tat]):
         self.testcases = testcases
         self.n_matchups = n_matchups
         self.n_rounds = n_rounds
 
-    def validate(self, bot):
-        raise NotImplementedError
+    def validate(self, bot: Bot) -> ResultDetails:
+        for testcase in self.testcases:
+            for _ in range(self.n_matchups):
+                history = []
+                for _ in range(self.n_rounds):
+                    start = time()
+
+                    try:
+                        history.append(bot(testcase, history))
+                    except Exception as e:
+                        return ResultDetails(Result.EXCEPTION, e)
+
+                    end = time()
+
+                    if end - start > 1:
+                        return ResultDetails(Result.TIMEOUT, None)
+
+        return ResultDetails(Result.VALID, None)
---
+++ pdarena_test_lib/test_validator.py
@@ -0,0 +1,25 @@
+from . import Validator, Result
+from time import sleep
+
+validator = Validator(10, 10)
+
+
+def test_valid():
+    def valid(opponent, history):
+        return False  # always cooperate
+
+    assert validator.validate(valid).result == Result.VALID
+
+
+def test_timeout():
+    def timeout(opponent, history):
+        sleep(1.1)
+
+    assert validator.validate(timeout).result == Result.TIMEOUT
+
+
+def test_exception():
+    def exception(opponent, history):
+        raise Exception
+
+    assert validator.validate(exception).result == Result.EXCEPTION
